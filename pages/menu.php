3778e7fbeb586064337923115a8231dcc7accce4
citra <citraangguncahyani1@gmail.com>
2025-11-11 14:42:00 +0700

    tambah tampilan menu makanan untuk citra

---
+++ pages/menu.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Menu Makanan - Resto Suttan</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+    <h2>🍽 Menu Makanan Resto Suttan</h2>
+
+    <div class="menu-container">
+        <div class="menu-item">
+            <img src="../assets/img/nasi-goreng.jpg" alt="Nasi Goreng">
+            <h3>Nasi Goreng</h3>
+            <p>Rp 25.000</p>
+            <button>Tambah ke Keranjang</button>
+        </div>
+
+        <div class="menu-item">
+            <img src="../assets/img/mie-goreng.jpg" alt="Mie Goreng">
+            <h3>Mie Goreng</h3>
+            <p>Rp 22.000</p>
+            <button>Tambah ke Keranjang</button>
+        </div>
+
+        <div class="menu-item">
+            <img src="../assets/img/ayam-bakar.jpg" alt="Ayam Bakar">
+            <h3>Ayam Bakar</h3>
+            <p>Rp 30.000</p>
+            <button>Tambah ke Keranjang</button>
+        </div>
+    </div>
+</body>
+</html>
