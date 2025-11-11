89fd4f43f376612be9bf18f59ed7541e6690191c
atarftcode <atarftcode@gmail.com>
2025-11-11 15:11:49 +0700

    menambahkan halaman dashboard admin untuk Resto Suttan

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suttan Resto</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+
+<header>
+    <div class="logo">
+        <img src="../assets/img/logo.png" alt="Logo Suttan Resto" width="80">
+        <h1>Suttan Resto</h1>
+    </div>
+    <nav>
+        <a href="../index.php">Beranda</a>
+        <a href="menu.php">Menu</a>
+        <a href="order.php">Pesan</a>
+        <a href="about.php">Tentang</a>
+        <a href="contact.php">Kontak</a>
+        <a href="../login.php" class="btn-login">Login</a>
+    </nav>
+</header>
+<hr>
