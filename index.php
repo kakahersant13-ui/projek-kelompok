15be3a2d362cfd73c25c3a45bbd1b7f92c7a2bdf
Kaka Hersant <kakahersant13@gmail.com>
2025-11-18 17:04:19 +0700

    Skema 2: tambah baris baru

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <main>
     <section class="hero">
         <h1>Selamat Datang di Suttan Resto 🍽️ - Kaka</h1>
+        <p>Makanan Indonesia dengan cita rasa khas dan suasana kekinian.</p>
         <p>Penambahan baru (Skema 2) oleh Kaka.</p>
         <a href="pages/menu.php" class="btn">Lihat Menu</a>
     </section>
