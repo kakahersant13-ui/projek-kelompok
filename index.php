491599923fa934e6cb2a622d11ce099f93dc17d1
rinasti <rinastiramadona@gmail.com>
2025-11-18 17:29:31 +0700

    Fix merge conflict index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include('includes/header.php'); ?>
 <main>
     <section class="hero">
-        <h1>Selamat Datang di Suttan Resto 🍽 - Kaka, Citra, Rinasti</h1>
+        <h1>Selamat Datang di Suttan Resto 🍽 - Kaka, Citra, Rinasti</h1> 
         <p>Makanan Indonesia dengan cita rasa khas dan suasana kekinian.</p>
         <p>Penambahan baru (Skema 2) oleh Rinasti.</p>
         <a href="pages/menu.php" class="btn">Lihat Menu</a>
